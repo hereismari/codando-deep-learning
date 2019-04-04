e8ca00feeb7bb760f90c1c34b70fdf2d54cb7a52
Marianne Linhares <mariannelinharesm@gmail.com>
2019-04-05 00:40:44 +0100

    Atualiza website

--- docs/js/docs.js
+++ docs/js/docs.js
@@ -1,3 +1,3 @@
-var notebooksArray = []
+var notebooksArray = ['intro_to_neural_nets', 'neural_nets']
 var notebooksDict = {'Depend-ncias-1': 'neural_nets'}
 var notebooksArrayMap = {'intro_to_neural_nets': 'Dependências', 'neural_nets': 'Dependências'}
