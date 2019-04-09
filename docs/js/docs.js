225a016b62f6a9edab718e1f74da023c730b5212
Marianne Linhares <mariannelinharesm@gmail.com>
2019-04-09 02:50:58 +0100

    Add intro_to_tensors.ipynb notebook

--- docs/js/docs.js
+++ docs/js/docs.js
@@ -1,3 +1,3 @@
-var notebooksArray = ['intro_to_neural_nets', 'neural_nets']
+var notebooksArray = ['intro_to_neural_nets', 'intro_to_tensors', 'neural_nets']
 var notebooksDict = {'Depend-ncias-1': 'neural_nets'}
-var notebooksArrayMap = {'intro_to_neural_nets': 'Dependências', 'neural_nets': 'Dependências'}
+var notebooksArrayMap = {'intro_to_neural_nets': 'Dependências', 'intro_to_tensors': 'Dependências', 'neural_nets': 'Dependências'}
