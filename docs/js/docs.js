bf7c3e20e8ceecb0bdcf178aa4510c2b534296f8
Arnaldo Gualberto <arnaldo.g12@gmail.com>
2019-04-10 13:33:26 -0300

    atualiza site

--- docs/js/docs.js
+++
@@ -1,3 +0,0 @@
-var notebooksArray = ['intro_to_neural_nets', 'intro_to_tensors', 'neural_nets']
-var notebooksDict = {'Depend-ncias-1': 'neural_nets'}
-var notebooksArrayMap = {'intro_to_neural_nets': 'Dependências', 'intro_to_tensors': 'Dependências', 'neural_nets': 'Dependências'}
