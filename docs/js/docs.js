ce059fa15f9b4b7db85c3ec21ee0509eb5e8732c
Marianne Linhares <mariannelinharesm@gmail.com>
2019-04-13 04:43:27 +0100

    Atualiza site

--- docs/js/docs.js
+++ docs/js/docs.js
@@ -1,3 +1,3 @@
-var notebooksArray = ['intro_to_neural_nets', 'intro_to_tensors', 'neural_nets']
-var notebooksDict = {'Depend-ncias-1': 'neural_nets'}
-var notebooksArrayMap = {'intro_to_neural_nets': 'Dependências', 'intro_to_tensors': 'Dependências', 'neural_nets': 'Dependências'}
+var notebooksArray = ['0. Introdução a Deep Learning', 'intro_to_neural_nets', 'intro_to_tensors', 'neural_nets']
+var notebooksDict = {'0.-Introdu-o-a-Deep-Learning-1': '0. Introdução a Deep Learning', '1.1-Introdu-o-a-Redes-Neurais:-O-que-s-o?-1': 'intro_to_neural_nets', 'Depend-ncias-1': 'neural_nets'}
+var notebooksArrayMap = {'0. Introdução a Deep Learning': '0. Introdução a Deep Learning', 'intro_to_neural_nets': '1.1 Introdução a Redes  ... : O que são?', 'intro_to_tensors': 'Dependências', 'neural_nets': 'Dependências'}
