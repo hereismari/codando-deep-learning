30ddab8c1338ed8774566a1439086a7c9baa0773
Marianne Linhares <mariannelinharesm@gmail.com>
2019-05-01 22:49:27 +0100

    Atualiza index

--- docs/js/docs.js
+++ docs/js/docs.js
@@ -1,3 +1,3 @@
-var notebooksArray = []
+var notebooksArray = ['0.intro_a_deep_learning', '1.1.historia_das_redes_neurais', '1.1.historia_das_redes_neurais', '1.3.do_que_se_alimentam', '1.4.como_aprendem', '1.5.minha_primeira_rede_neural']
 var notebooksDict = {'0.-Introdu-o-a-Deep-Learning-1': '0.intro_a_deep_learning', '1.1-Introdu-o-a-Redes-Neurais:-O-que-s-o?-1': '1.2.o_que_sao', 'import-numpy-as-np-1': '1.3.do_que_se_alimentam', '1.3-Codando-um-MLP-(MultiLayerPerceptron)-1': '1.5.minha_primeira_rede_neural'}
 var notebooksArrayMap = {'0.intro_a_deep_learning': '0. Introdução a Deep Learning', '1.1.historia_das_redes_neurais': '1.1 Introdução a Redes  ... : O que são?', '1.2.o_que_sao': '1.1 Introdução a Redes  ... : O que são?', '1.3.do_que_se_alimentam': 'import numpy as np', '1.4.como_aprendem': '1.3 Codando um MLP (Mul ... rPerceptron)', '1.5.minha_primeira_rede_neural': '1.3 Codando um MLP (Mul ... rPerceptron)'}
