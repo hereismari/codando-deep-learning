fd965763706420a536b0d554e42e46776341deb4
Marianne Linhares <mariannelinharesm@gmail.com>
2019-05-01 22:39:29 +0100

    Atualiza site

--- docs/js/docs.js
+++ docs/js/docs.js
@@ -1,3 +1,3 @@
-var notebooksArray = ['0. Introdução a Deep Learning', 'intro_to_neural_nets', 'intro_to_tensors', 'neural_nets']
-var notebooksDict = {'0.-Introdu-o-a-Deep-Learning-1': '0. Introdução a Deep Learning', '1.1-Introdu-o-a-Redes-Neurais:-O-que-s-o?-1': 'intro_to_neural_nets', 'Depend-ncias-1': 'neural_nets'}
-var notebooksArrayMap = {'0. Introdução a Deep Learning': '0. Introdução a Deep Learning', 'intro_to_neural_nets': '1.1 Introdução a Redes  ... : O que são?', 'intro_to_tensors': 'Dependências', 'neural_nets': 'Dependências'}
+var notebooksArray = []
+var notebooksDict = {'0.-Introdu-o-a-Deep-Learning-1': '0.intro_a_deep_learning', '1.1-Introdu-o-a-Redes-Neurais:-O-que-s-o?-1': '1.2.o_que_sao', 'import-numpy-as-np-1': '1.3.do_que_se_alimentam', '1.3-Codando-um-MLP-(MultiLayerPerceptron)-1': '1.5.minha_primeira_rede_neural'}
+var notebooksArrayMap = {'0.intro_a_deep_learning': '0. Introdução a Deep Learning', '1.1.historia_das_redes_neurais': '1.1 Introdução a Redes  ... : O que são?', '1.2.o_que_sao': '1.1 Introdução a Redes  ... : O que são?', '1.3.do_que_se_alimentam': 'import numpy as np', '1.4.como_aprendem': '1.3 Codando um MLP (Mul ... rPerceptron)', '1.5.minha_primeira_rede_neural': '1.3 Codando um MLP (Mul ... rPerceptron)'}
